4d510edbfe7b0e3d6a92ad598d50cff9f1b023bd
TomaszLizer <tomasz.lizer@anywhere.co>
2025-06-13 14:47:00 +0200

    Initial Commit

---
+++ Task explorations/Task_explorationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_explorationsApp.swift
+//  Task explorations
+//
+//  Created by Tomasz Lizer on 13/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Task_explorationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
